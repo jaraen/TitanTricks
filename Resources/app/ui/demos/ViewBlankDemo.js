b56c672069b8f820e4f4106319199865c9d2a960
Javier Rayon <javier@criteriastudio.com>
2011-10-17 20:23:32 +0200

    new file: template for demos

---
+++ Resources/app/ui/demos/ViewBlankDemo.js
@@ -0,0 +1,32 @@
+//Use this file as template for new demos
+
+//replace this function with your own method identifier
+App.ui.demos.createViewBlank = {};
+
+(function(){
+	
+	//replace this function with your own method identifier
+	App.ui.demos.createViewBlank = function(){
+		var view = App.ui.createViewDemo(demoInfo);
+		
+		
+		return view;
+	};
+
+	//Standard demo declaration used along TitanTricks App to reference each demo.
+	var demoInfo = {
+		title:			'',
+		description: 	'',
+		instructions: 	'',
+		component: 		'',
+		header: 		'UI',
+		ios:			true,
+		android:		true,
+		createView: 	App.ui.demos.createViewBlank //Replace by your creator function
+	}
+	
+	//UNCOMMENT THIS LINE. IS COMMENTED TO AVOID THE BLANK VIEW APPEARS IN THE DEMOS INDEX
+	//add demo info to the array used to fill the index table view.
+	//App.ui.demos.list.push(demoInfo);
+
+})();
