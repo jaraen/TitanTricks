2ccb27aa34b6a70dfe32f6f1f9be2568e27e07ff
Javier Rayon <javier@criteriastudio.com>
2012-04-22 16:53:30 +0200

    Refactored to commonJS and TItanium 2 compatibility added

---
+++ Resources/ModulePaths.js
@@ -0,0 +1,32 @@
+
+var ui = '/app/ui/';
+var components = '/app/ui/components/';
+
+module.exports = {
+	//App components
+	APPWINDOW: ui + 'AppWindow',
+	INDEXVIEW: ui + 'ViewIndex',
+	INDEXROWVIEW: ui + 'RowIndex',	
+	DEMOWIN: ui + 'WinDemo',
+	MAINVIEW: ui + 'MainView',
+	TEMPLATEDEMO: ui + 'ViewTemplateDemo',
+	
+	//Others
+	TOOLS: '/app/Tools',
+	STYLES: ui + 'styles',
+	
+	//Demos "proxy"
+	DEMOSLIST: ui + 'demos/demos',
+	
+	//Reusable components
+	BADGEIMAGE: components + 'BadgeImage',
+	COLLAPSIBLEVIEW: components + 'CollapsibleView',
+	FLIPIMAGE: components + 'FlipImage',
+	IMAGERESIZER: components + 'ImageResizer',
+	JOYSTICK: components + 'JoystickController',
+	LONGCLICKBUTTON: components + 'LongClickButton',
+	PROGRESSBAR: components + 'ProgressBar',
+	REFLECTIMAGE: components + 'ReflectImage',
+	ROWCOLUMNVIEW: components + 'RowColumnView',
+	SWITCHBUTTON: components + 'SwitchButton'
+}
